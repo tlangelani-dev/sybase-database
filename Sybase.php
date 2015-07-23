6115f513b02e762be53883768e1364f549cb8dc5
Tlangelani Mboweni <mbowenit@gmail.com>
2015-07-23 12:58:25 +0200

    update Sybase fetch type

--- Sybase.php
+++ Sybase.php
@@ -5,6 +5,9 @@ namespace Tlangelani\Database;
 class Sybase {
     
     protected $conn;
+    protected $data = array();
+    private $fetchTypes = array('ASSOC', 'ARRAY', 'OBJ', 'FIELD', 'ROW');
+    private $result;
     
     private $dbHost;
     private $dbName;
@@ -12,27 +15,94 @@ class Sybase {
     private $dbPass;
     
     public function __construct($dbHost, $dbName, $dbUser, $dbPass) {
+        
         $this->dbHost = $dbHost;
         $this->dbName = $dbName;
         $this->dbUser = $dbUser;
         $this->dbPass = $dbPass;
-        $this->conn = sybase_connect($this->dbHost, $this->dbUser, $this->dbPass);
-        if (!$this->conn) {
-            trigger_error("Sybase Connection Error: " . sybase_get_last_message());
-        }
+        
+        $this->connect($this->dbHost);
         $this->selectDB($this->dbName);
     }
     
+    /**
+     * Select database.
+     * @param string $db
+     * @return boolean
+     */
     public function selectDB($db) {
         $this->dbName = $db;
         return sybase_select_db($this->dbName, $this->conn);
     }
     
-    public function connect() {
-        
+    /**
+     * Connect to Sybase database server.
+     * @param type $host
+     * @return resource
+     */
+    public function connect($host) {
+        $this->dbHost = $host;
+        $this->conn = sybase_connect($this->dbHost, $this->dbUser, $this->dbPass);
+        if (!$this->conn) {
+            trigger_error("Sybase Connection Error: " . sybase_get_last_message());
+        }
+        return $this->conn;
     }
     
-    public function query() {
+    public function query($sql, $fetchType = 'ASSOC') {
         
+        // clear data array
+        $this->data = array();
+        
+        // send sql query to database
+        $this->result = sybase_query($sql, $this->conn);
+        
+        // fetch results
+        if (is_resource($this->result)) {
+            $this->fetchResults($fetchType);
+        }
+        
+        return $this->data;
     }
+    
+    /**
+     * This function will fetch results based on user fetch type.
+     * @param string $fetchType
+     */
+    protected function fetchResults($fetchType) {
+        
+        // check if user fetch type is valid
+        if (!in_array($fetchType, $this->fetchTypes)) {
+            trigger_error('Sybase Error: Invalid query fetch type: ' . $fetchType);
+        }
+        
+        switch($fetchType) {
+            case 'ASSOC':
+                while($row = sybase_fetch_assoc($this->result)) {
+                    $this->data[] = $row;
+                }
+                break;
+            case 'ARRAY':
+                while($row = sybase_fetch_array($this->result)) {
+                    $this->data[] = $row;
+                }
+                break;
+            case 'OBJ':
+                while($row = sybase_fetch_object($this->result)) {
+                    $this->data[] = $row;
+                }
+                break;
+            case 'FIELD':
+                while($row = sybase_fetch_field($this->result)) {
+                    $this->data[] = $row;
+                }
+                break;
+            case 'ROW':
+                while($row = sybase_fetch_row($this->result)) {
+                    $this->data[] = $row;
+                }
+                break;
+        }
+    }
+    
 }
